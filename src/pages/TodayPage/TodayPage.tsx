1c74ef95967a338db125548232c18bcb26601357
Dobrunia Kostrigin <48620984+Dobrunia@users.noreply.github.com>
2025-04-18 23:17:06 +0300

    dev

--- src/pages/TodayPage/TodayPage.tsx
+++ src/pages/TodayPage/TodayPage.tsx
@@ -1,31 +1,14 @@
 import { useAppSelector } from '../../hooks/storeHooks';
 import { TaskSection } from '../../modules/TaskSection/TaskSection';
-import {
-  selectTasksLoading,
-  selectTasksError,
-  selectTaskIdsByDateIncludingUndated,
-} from '../../entities/task/store/tasksSlice';
-import React from 'react';
+import { selectTaskIdsByDateIncludingUndated } from '../../entities/task/store/tasksSlice';
 
-export const TodayPage: React.FC = React.memo(() => {
+export const TodayPage = () => {
   // Получаем сегодняшнюю дату в формате YYYY-MM-DD
   const today = new Date().toLocaleDateString('sv-SE');
 
   // Получаем ID задач на сегодня через селектор
   const todayTaskIds = useAppSelector((state) => selectTaskIdsByDateIncludingUndated(state, today));
-
-  // Получаем статус загрузки и ошибки из Redux
-  const isLoading = useAppSelector(selectTasksLoading);
-  const error = useAppSelector(selectTasksError);
-
-  // Отображаем секцию задач, передавая идентификаторы задач
-  return (
-    <>
-      {isLoading && <div>Загрузка задач...</div>}
-      {error && <div>Ошибка: {error}</div>}
-      <TaskSection date={today} taskIds={todayTaskIds} />
-    </>
-  );
-});
+  return <TaskSection date={today} taskIds={todayTaskIds} />;
+};
 
 TodayPage.displayName = 'TodayPage';
